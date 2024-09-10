ef7122c229c70433cad49a1d1e1a7f76f71dcad8
andremc331 <andremc331@gmail.com>
2024-09-10 20:56:22 -0300

    modified:   front/src/App.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+//import Saida from "./Saida";
+//import Entrada from "./Entrada"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+  const [nome, setNome] = useState("");
+  const [lista, setLista] = useState<string []>([]);
+
+  function handle(){
+    const temp =[...lista,nome]; //se colocar nome antes dos ... o nome será salvo sempre em 1°
+    setLista(temp);
+  }
+
+
+  return ( //sempre q houver onClick necessário função
+    <div>
+      <div>
+        <label>Exercício 3</label>
+        <input value={nome} onChange={(e)=>setNome(e.target.value)} />
+        <button onClick={handle}>Salvar</button>
+      </div>
     </div>
   );
 }
