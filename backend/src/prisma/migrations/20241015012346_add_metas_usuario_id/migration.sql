8e9dbd9a9421ef33b9fc34b532da37787f460041
andremc331 <andremc331@gmail.com>
2024-10-14 22:31:20 -0300

    backend

--- backend/src/prisma/migrations/20241015012346_add_metas_usuario_id/migration.sql
+++ backend/src/prisma/migrations/20241015012346_add_metas_usuario_id/migration.sql
@@ -3,20 +3,15 @@
 
   - You are about to drop the column `ind_imc` on the `Conta` table. All the data in the column will be lost.
   - You are about to drop the column `metas` on the `Meta` table. All the data in the column will be lost.
-  - You are about to drop the column `metas_usuario_id` on the `Meta` table. All the data in the column will be lost.
   - You are about to drop the column `pesagem` on the `Peso` table. All the data in the column will be lost.
   - You are about to drop the column `peso_usuario_id` on the `Peso` table. All the data in the column will be lost.
   - You are about to drop the column `altura` on the `Usuario` table. All the data in the column will be lost.
   - You are about to drop the column `idade` on the `Usuario` table. All the data in the column will be lost.
   - You are about to drop the column `nome` on the `Usuario` table. All the data in the column will be lost.
   - You are about to drop the column `peso` on the `Usuario` table. All the data in the column will be lost.
-  - Added the required column `usuarioId` to the `Meta` table without a default value. This is not possible if the table is not empty.
   - Added the required column `usuario_id` to the `Peso` table without a default value. This is not possible if the table is not empty.
 
 */
--- DropForeignKey
-ALTER TABLE "Meta" DROP CONSTRAINT "Meta_metas_usuario_id_fkey";
-
 -- DropForeignKey
 ALTER TABLE "Peso" DROP CONSTRAINT "Peso_peso_usuario_id_fkey";
 
@@ -25,9 +20,7 @@ ALTER TABLE "Conta" DROP COLUMN "ind_imc";
 
 -- AlterTable
 ALTER TABLE "Meta" DROP COLUMN "metas",
-DROP COLUMN "metas_usuario_id",
-ADD COLUMN     "descricao" TEXT,
-ADD COLUMN     "usuarioId" INTEGER NOT NULL;
+ADD COLUMN     "descricao" TEXT;
 
 -- AlterTable
 ALTER TABLE "Peso" DROP COLUMN "pesagem",
@@ -40,9 +33,6 @@ DROP COLUMN "idade",
 DROP COLUMN "nome",
 DROP COLUMN "peso",
 ALTER COLUMN "senha" SET DATA TYPE TEXT;
-
--- AddForeignKey
-ALTER TABLE "Meta" ADD CONSTRAINT "Meta_usuarioId_fkey" FOREIGN KEY ("usuarioId") REFERENCES "Usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Peso" ADD CONSTRAINT "Peso_usuario_id_fkey" FOREIGN KEY ("usuario_id") REFERENCES "Usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- backend/src/services/metaService.ts
+++ backend/src/services/metaService.ts
@@ -1,18 +1,16 @@
-// src/services/metaService.ts
 import prisma from "../prisma/prismaClient";
 
 // Função para criar uma nova meta
 export const criarMeta = async (descricao: string, usuarioId: number) => {
   try {
     // Inserir a meta no banco de dados usando o Prisma
-    const novoMeta = await prisma.meta.create({
+    const novaMeta = await prisma.meta.create({
       data: {
-        metas_usuario_id: usuarioId, // Supondo que você tenha um usuarioId
-        metas: "Defina sua meta", // Exemplo de meta
+        metas_usuario_id: usuarioId, // Usando metas_usuario_id como chave estrangeira
         descricao: descricao // Aqui é onde você usa o parâmetro recebido
       }
     });
-    return novoMeta; // Retorne o novo registro criado
+    return novaMeta; // Retorne o novo registro criado
   } catch (error) {
     console.error("Erro ao criar meta:", error);
     throw error;
