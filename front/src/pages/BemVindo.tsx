b6281cbfd2eae90cc0d0525f9ceba7b3c9386e5d
Andre Ventura <andrehventura21@gmail.com>
2024-11-06 15:48:13 -0300

    styled-login

--- front/src/pages/BemVindo.tsx
+++ front/src/pages/BemVindo.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import styled_BemV from '../styled/styled_BemV';
-import logo from "../assets/logo.nutritech.png"
-// import { Icon } from "../styled/styled_Main";
-// import { IonIcon } from "@ionic/react";
-// import { Icons } from "../components/icons";
-import { useNavigate } from 'react-router-dom';
-import { api } from '../services/api';
+import React, { useState } from "react";
+import styled_BemV from "../styled/styled_BemV";
+import logo from "../assets/logo.nutritech.png";
+import { Icon } from "../styled/styled_Main";
+import { IonIcon } from "@ionic/react";
+import { Icons } from "../components/icons";
+import { useNavigate } from "react-router-dom";
+import { api } from "../services/api";
 
 const {
   Background,
@@ -22,72 +22,79 @@ const {
 } = styled_BemV();
 
 const BemVindo: React.FC = () => {
-  const [email, setEmail] = useState<string>('');
-  const [senha, setSenha] = useState<string>('');
+  const [email, setEmail] = useState<string>("");
+  const [senha, setSenha] = useState<string>("");
   const navigate = useNavigate();
 
   const handleLogin = async () => {
     // Verifica se os campos de email e senha estão preenchidos
     if (!email || !senha) {
-      alert('Por favor, preencha todos os campos.');
+      alert("Por favor, preencha todos os campos.");
       return; // Impede que a função continue se os campos estiverem vazios
     }
-  
+
     try {
       // Chama a API de login
-      const response = await api.post('/login', { email: email, senha: senha });
-  
+      const response = await api.post("/login", { email: email, senha: senha });
+
       // Verifica se a resposta contém um token de acesso
       if (response.data.token) {
         // Salva o token no localStorage
-        localStorage.setItem('token', response.data.token);
+        localStorage.setItem("token", response.data.token);
         // Redireciona para a página home
-        navigate('/home');
+        navigate("/home");
       } else {
         // Caso o login falhe, exibe uma mensagem de erro
-        alert('Email ou senha incorretos.');
+        alert("Email ou senha incorretos.");
       }
     } catch (error) {
-      console.error('Erro ao fazer login:', error);
-      alert('Erro ao efetuar o login. Tente novamente.');
+      console.error("Erro ao fazer login:", error);
+      alert("Erro ao efetuar o login. Tente novamente.");
     }
   };
-  
 
   return (
     <Background>
       <ContainerLeft>
-      <link href="https://fonts.googleapis.com/css2?family=Playpen+Sans&display=swap" rel="stylesheet"></link>
         <h1>Login</h1>
         <EmailLabel>Email:</EmailLabel>
-        <Input 
-          type="email" 
-          className="email-input" 
+        <Input
+          type="email"
+          className="email-input"
           value={email}
-          onChange={(e) => setEmail(e.target.value)} 
+          onChange={(e) => setEmail(e.target.value)}
         />
         <PasswordLabel>Senha:</PasswordLabel>
-        <Input 
-          type="password" 
-          className="password-input" 
+        <Input
+          type="password"
+          className="password-input"
           value={senha}
-          onChange={(e) => setSenha(e.target.value)} 
+          onChange={(e) => setSenha(e.target.value)}
         />
         <MainContent>
-          <Button2 onClick={handleLogin}>&gt;</Button2>
+          <Button2 onClick={handleLogin}>
+            <Icon>
+              <IonIcon icon={Icons.chevronForward} />
+            </Icon>
+          </Button2>
         </MainContent>
       </ContainerLeft>
 
       <ContainerRightTitle>
         <Logo src={logo} alt="Logo NutriTech" />
         <JustifiedText>
-          O MELHOR
+          O melhor
           <br />
-          PARA A SUA
+          Para a sua
           <br />
-          SAÚDE!
+          Saúde!
         </JustifiedText>
-        <Button1 onClick={() => navigate('/cadastro')}>COMECE JÁ</Button1>
+        <Button1 onClick={() => navigate("/cadastro")}>
+          COMECE JÁ
+          <Icon>
+            <IonIcon icon={Icons.arrowForward} />
+          </Icon>
+        </Button1>
       </ContainerRightTitle>
     </Background>
   );
